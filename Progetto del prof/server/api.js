5658db07d314c93b5b94b8aa3cf0be1de661203f
CarloAmbrogi <carlo.ambrogi@mail.polimi.it>
2022-04-27 16:10:03 +0200

    started with project

---
+++ Progetto del prof/server/api.js
@@ -0,0 +1,213 @@
+const express = require('express')
+const app = express()
+const { Sequelize, DataTypes } = require("sequelize")
+const initialize = require('./initialize').default
+app.use(express.json())
+
+const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp")
+
+// Function that will initialize the connection to the database
+async function initializeDatabaseConnection() {
+    await database.authenticate()
+    const Event = database.define("event", {
+        name: DataTypes.STRING,
+        date: DataTypes.DATE,
+        location: DataTypes.STRING,
+        price: DataTypes.INTEGER,
+        description: DataTypes.STRING,
+        images: DataTypes.ARRAY(DataTypes.STRING),
+        videos: DataTypes.ARRAY(DataTypes.STRING),
+        shortDescription: DataTypes.STRING,
+    })
+    const PointOfInterest = database.define("pointOfInterest", {
+        name: DataTypes.STRING,
+        location: DataTypes.STRING,
+        times: DataTypes.STRING,
+        price: DataTypes.INTEGER,
+        description: DataTypes.STRING,
+        images: DataTypes.ARRAY(DataTypes.STRING),
+        videos: DataTypes.ARRAY(DataTypes.STRING),
+        shortDescription: DataTypes.STRING,
+    })
+    await database.sync({ force: true })
+    return {
+        Event, PointOfInterest
+    }
+}
+
+/*
+// Function that will initialize the connection to the database
+async function initializeDatabaseConnection() {
+    await database.authenticate()
+    const Cat = database.define("cat", {
+        name: DataTypes.STRING,
+        description: DataTypes.STRING,
+        breed: DataTypes.STRING,
+        img: DataTypes.STRING,
+    })
+    await database.sync({ force: true })
+    return {
+        Cat
+    }
+}
+*/
+
+// With this line, our server will know how to parse any incoming request
+// that contains some JSON in the body
+
+const pageContentObject = {
+    index: {
+        title: "TownTitle",
+        image: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
+        shortOverview: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,
+        history: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,
+        whyVisitThisTown: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,
+        photoLibrary: {
+            image1: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
+            image2: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
+            image3: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
+            image4: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
+            image5: "https://fs.i3lab.group/hypermedia/images/index.jpeg"
+        }
+    },
+}
+
+/*
+const pageContentObject = {
+    index: {
+        title: "Homepage",
+        image: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`
+    },
+    about: {
+        title: "About",
+        image: "https://fs.i3lab.group/hypermedia/images/about.jpeg",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`
+    },
+}
+*/
+
+async function runMainApi() {
+    const models = await initializeDatabaseConnection()
+    await initialize(models)
+
+    app.get('/page-info/:topic', (req, res) => {
+        const { topic } = req.params
+        const result = pageContentObject[topic]
+        return res.json(result)
+    })
+
+    app.get('/event/:id', async (req, res) => {
+        const id = +req.params.id
+        const result = await models.Event.findOne({ where: { id } })
+        return res.json(result)
+    })
+
+    app.get('/pointOfInterest/:id', async (req, res) => {
+        const id = +req.params.id
+        const result = await models.PointOfInterest.findOne({ where: { id } })
+        return res.json(result)
+    })
+
+    // HTTP GET api that returns all the cats in our fake database
+    app.get("/event", async (req, res) => {
+        const result = await models.Event.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                name: element.name,
+                date: element.date,
+                location: element.location,
+                price: element.price,
+                description: element.description,
+                images: element.images,
+                videos: element.videos,
+                shortDescription: element.shortDescription,
+                id: element.id,
+            })
+        }
+        return res.json(filtered)
+    })
+
+    // HTTP GET api that returns all the cats in our fake database
+    app.get("/pointOfInterest", async (req, res) => {
+        const result = await models.PointOfInterest.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                name: element.name,
+                location: element.location,
+                times: element.times,
+                price: element.price,
+                description: element.description,
+                images: element.images,
+                videos: element.videos,
+                shortDescription: element.shortDescription,
+                id: element.id,
+            })
+        }
+        return res.json(filtered)
+    })
+
+    /*
+    // HTTP POST apio that will push (and therefore create) a new element in 
+    // our fake database 
+    app.post("/cats", (req, res) => {
+        const { body } = req
+        catList.push(body)
+        return res.sendStatus(200)
+    })
+    */
+}
+
+/*
+async function runMainApi() {
+    const models = await initializeDatabaseConnection()
+    await initialize(models)
+
+    app.get('/page-info/:topic', (req, res) => {
+        const { topic } = req.params
+        const result = pageContentObject[topic]
+        return res.json(result)
+    })
+
+    app.get('/cats/:id', async (req, res) => {
+        const id = +req.params.id
+        const result = await models.Cat.findOne({ where: { id } })
+        return res.json(result)
+    })
+
+    // HTTP GET api that returns all the cats in our fake database
+    app.get("/cats", async (req, res) => {
+        const result = await models.Cat.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                name: element.name,
+                img: element.img,
+                breed: element.breed,
+                id: element.id,
+            })
+        }
+        return res.json(filtered)
+    })
+
+    // HTTP POST apio that will push (and therefore create) a new element in 
+    // our fake database 
+    app.post("/cats", (req, res) => {
+        const { body } = req
+        catList.push(body)
+        return res.sendStatus(200)
+    })
+}
+*/
+
+runMainApi()
+
+
+export default app
