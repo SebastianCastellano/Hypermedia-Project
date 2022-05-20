53e89927de9c1dec62bae8b4b09b6ce62856b33e
Lorenzo-Guerrieri-98 <lorenzo.guerrieri@mail.polimi.it>
2022-05-20 22:54:09 +0200

    Add fixed scrollable header

--- Il nostro progetto/components/TheHeader.vue
+++ Il nostro progetto/components/TheHeader.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
+  <div id="mainDiv">
     <nav class="navbar navbar-expand-lg navbar-light px-3 header">
-      <nuxt-link class="navbar-brand" id="home-link" to="/">
+      <nuxt-link id="home-link" class="navbar-brand" to="/">
         <span class="capital-letter">V</span>isit<span class="capital-letter">M</span>antova
       </nuxt-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+      <button 
+        class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
         aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarToggler">
+      <div id="navbarToggler" class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           
           <div>
@@ -20,17 +21,19 @@
             <!---->
 
             <div class="nav-link dropdown-div">
-              <nuxt-link class="" :to="events[0].path"  id="dropdown-list">
+              <nuxt-link id="dropdown-list" class="" :to="events[0].path">
                 {{ events[0].name }}
               </nuxt-link>
-              <button type="button" class="
+              <button
+                  type="button" class="
                   btn btn-sm btn-secondary
                   dropdown-toggle dropdown-toggle-split
                 " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 
               </button>
               <div class="dropdown-menu">
-                <nuxt-link v-for="(eventItem, eventItemIndex) of eventList" :key="`eventItem${eventItemIndex}`"
+                <nuxt-link
+                  v-for="(eventItem, eventItemIndex) of eventList" :key="`eventItem${eventItemIndex}`"
                   class="dropdown-item" :to="eventItem.path">{{ eventItem.name }}</nuxt-link>
               </div>
             </div>
@@ -60,6 +63,10 @@ a {
   padding-top: 0px;
   padding-bottom: 0px;
   background: var(--color1);
+  width: 100%;
+  top: 0;
+  position: fixed;
+  z-index: 5;
 }
 
 #home-link {
