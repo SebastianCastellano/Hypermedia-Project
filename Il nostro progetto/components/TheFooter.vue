c1b7f18c65288402d07315acb0448d7604be0367
Carlo Ambrogi <carlomaria.ambrogi@icloud.com>
2022-05-28 19:08:52 +0200

    Updated the footer

--- Il nostro progetto/components/TheFooter.vue
+++ Il nostro progetto/components/TheFooter.vue
@@ -5,6 +5,9 @@
                 Home page
             </nuxt-link>
         </div>
+        <div class="team-info">
+            Softmath --- 10612772 - Carlo Ambrogi --- 10582405 - Sebastian Castellano --- 10573185 - Andrea De Bettin --- 10571386 - Lorenzo Guerrieri
+        </div>
     </footer>
 </template>
 
@@ -37,6 +40,20 @@ div {
   top: 50%;
   -ms-transform: translateY(-50%);
   transform: translateY(-50%);
+}
+
+.team-info {
+  position: absolute;
+  bottom: 10%;
+  right: 25px;
+  border: 3px solid #73AD21;
+  border-style: dashed;
+/*
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+*/
+  color: lightgrey;
+  max-width: 80%;
 }
 
 </style>
